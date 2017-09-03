277b4967ba510745f7e3b938b1e21adaad4781ad
clairethebear <claire.oubelaid@gmail.com>
2017-09-03 17:26:52 +1000

    Kubestats first commit

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import socket
+import os
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    getcwd = os.getcwd()
+    top = subprocess.check_output("ps", shell=True)
+    output = 'Hostname: %s \n Top: %s' % (socket.gethostname(), top)
+    return output
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
