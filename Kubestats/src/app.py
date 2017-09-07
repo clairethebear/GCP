2d13aa186ce4d3ca7980bde566733a46e91ea86d
Claire Oubelaid <coubelaid@coubelaid.syd.corp.google.com>
2017-09-07 16:55:05 +1000

    Update to app.py

--- Kubestats/src/app.py
+++ Kubestats/src/app.py
@@ -6,11 +6,18 @@ import subprocess
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def kubestats():
     getcwd = os.getcwd()
+    node_ip = subprocess.check_output("curl http://169.254.169.254/0.1/meta-data/network", shell=True)
+    node_ip = format_ip(node_ip)
     top = subprocess.check_output("ps", shell=True)
-    output = 'Newest \nHostname: %s \n Top: %s' % (socket.gethostname(), top)
+    output = 'Newest \nHostname: %s \n Top: %s \n\n Node IP: %s\n\n' % (socket.gethostname(), top, node_ip)
     return output
+
+def format_ip(node_ip):
+    split_by_name = node_ip.split("externalIp\":\"", 1)[1]
+    split_by_name = split_by_name.split("\",", 1)[0]
+    return split_by_name
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
